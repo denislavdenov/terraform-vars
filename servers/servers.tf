d46f3cfa04048cae3c05e5ac1f65597d0fdc39e4
Denislav Denov <denislavdenov@Denislavs-MacBook-Pro.local>
2018-08-27 10:13:52 +0300

    edit

--- servers/servers.tf
+++ servers/servers.tf
@@ -4,7 +4,7 @@ resource "aws_instance" "example" {
   ami           		= "${var.ami}"
   instance_type 		= "${var.instance_type}"
 }
-output "93public_ip"
+output "public_ip"
 {
 value = "$(aws_instance.example.public_ip)"
 
