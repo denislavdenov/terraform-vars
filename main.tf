7e330791018abe5d6f8a646b55a021abd68b68d3
Denislav Denov <denislavdenov@Denislavs-MacBook-Pro.local>
2018-08-27 16:29:12 +0300

    finalyworking

--- main.tf
+++ main.tf
@@ -1,10 +1,11 @@
 module "servers" {
   source = "servers"
 
-  ami           = "${var.ami}"
-  instance_type = "${var.instance_type}"
-  public_key    = "${var.public_key}"
-  identity      = "${var.identity}-key"
+  ami               = "${var.ami}"
+  instance_type     = "${var.instance_type}"
+  public_key        = "${var.public_key}"
+  identity          = "${var.identity}-key"
+  security_group_id = "${var.security_group_id}"
 }
 
 output "public_ip" {
--- servers/servers.tf
+++ servers/servers.tf
@@ -2,6 +2,7 @@ variable "ami" {}
 variable "instance_type" {}
 variable "identity" {}
 variable "public_key" {}
+variable "security_group_id" {}
 
 resource "aws_key_pair" "training" {
   key_name   = "${var.identity}-key"
@@ -9,9 +10,20 @@ resource "aws_key_pair" "training" {
 }
 
 resource "aws_instance" "example" {
-  ami           = "${var.ami}"
-  instance_type = "${var.instance_type}"
-  key_name      = "${aws_key_pair.training.id}"
+  ami                    = "${var.ami}"
+  instance_type          = "${var.instance_type}"
+  key_name               = "${aws_key_pair.training.id}"
+  vpc_security_group_ids = ["${var.security_group_id}"]
+
+  connection {
+    user        = "ubuntu"
+    private_key = "${file("~/.ssh/id_rsa")}"
+  }
+
+  provisioner "file" {
+    source      = "/Users/denislavdenov/Documents/fromhost"
+    destination = "/tmp/fromhost"
+  }
 }
 
 output "public_ip" {
