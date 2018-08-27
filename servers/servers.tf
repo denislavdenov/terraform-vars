e7ce6e418cde63b96c583d83fcda54eace16e155
Denislav Denov <denislavdenov@Denislavs-MacBook-Pro.local>
2018-08-27 10:34:05 +0300

    fmt

--- servers/servers.tf
+++ servers/servers.tf
@@ -2,12 +2,10 @@ variable "ami" {}
 variable "instance_type" {}
 
 resource "aws_instance" "example" {
-  ami           		= "${var.ami}"
-  instance_type 		= "${var.instance_type}"
+  ami           = "${var.ami}"
+  instance_type = "${var.instance_type}"
 }
 
-output "public_ip"
-{
-value = "$(aws_instance.example.public_ip)"
-
+output "public_ip" {
+  value = "$(aws_instance.example.public_ip)"
 }
